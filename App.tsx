f5112fd7c30c87a9f324fe307078b10a520c5e33
Yena-Yun <cheryleduart@gmail.com>
2023-04-30 23:00:37 +0900

    feat: Dimension으로 화면 너비 가져오기, ScrollView의 여러 props 실습

--- App.tsx
+++ App.tsx
@@ -1,47 +1,123 @@
-import { Text, View, StatusBar, Image } from 'react-native';
+import {
+  Text,
+  View,
+  StatusBar,
+  Image,
+  ScrollView,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 import { Date, Icon, Const } from 'utils';
-import { useWeather } from 'hooks/useWeather';
 import { COLOR } from 'styles/color';
-import { styles } from 'styles/AppStyle';
+import { useWeather } from 'hooks/useWeather';
+import { useState } from 'react';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
+console.log('SCREEN_WIDTH: ' + SCREEN_WIDTH);
 
 export default function App() {
-  const [weatherData, isLoading, error] = useWeather();
-
-  const {
-    clouds,
-    temp,
-    tempMin,
-    tempMax,
-    weather,
-    weatherIcon,
-    rain,
-    snow,
-    wind,
-  } = weatherData;
+  const [
+    { clouds, temp, tempMin, tempMax, weather, weatherIcon, rain, snow, wind },
+    isLoading,
+    error,
+  ] = useWeather();
+  const [tempColor, setTempColor] = useState(styles.initialTemp);
 
   const weatherIconUrl = `https://openweathermap.org/img/w/${weatherIcon}.png`;
 
+  const handleMomentumScrollBegin = () => {
+    setTempColor(styles.updatedTemp);
+  };
+
+  const handleMomentumScrollEnd = () => {
+    setTempColor(styles.initialTemp);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.statusbar}>
+      <View style={styles.header}>
         {Icon.BARS_ICON}
         <Text style={styles.city}>{Const.CITY_NAME}</Text>
         {Icon.SEARCH_ICON}
       </View>
       <View style={styles.dates}>
         <Text style={styles.day}>{Date.DAY}</Text>
-        <Text style={{ fontSize: 16 }}>
+        <Text>
           {Date.MONTH}&nbsp;
           {Date.DATE}일
         </Text>
       </View>
-      <View style={styles.main}>
-        <Text style={styles.temp}>{temp}°</Text>
-        <View style={styles.weatherWrap}>
-          <Text style={styles.weather}>{weather}</Text>
-          <Image style={styles.weatherIcon} source={{ uri: weatherIconUrl }} />
+      <ScrollView
+        contentContainerStyle={styles.contentContainer}
+        fadingEdgeLength={300}
+        horizontal
+        // 스크롤 후 손가락 떼고 나서 살짝 추가로 m밀릴 때 이벤트 발생
+        // onMomentumScrollBegin={handleMomentumScrollBegin}
+        // onMomentumScrollEnd={handleMomentumScrollEnd}
+        // 정확히 스크롤을 하고 있는 동안(= 스크롤 시작 후 손가락을 대고 있을 동안) 이벤트 발생
+        onScrollBeginDrag={handleMomentumScrollBegin}
+        onScrollEndDrag={handleMomentumScrollEnd}
+        pagingEnabled
+        // persistentScrollbar // false이면 스크롤 안 할 때도 스크롤바가 계속 떠 있고 true면 스크롤할 때만 잠깐 보임
+        showsHorizontalScrollIndicator={false} // false하면 스크롤 여부에 상관없이 스크롤바 아예 안 보임
+        // snapToEnd={false}
+        // snapToInterval={4}
+        stickyHeaderIndices={[0]}
+        stickyHeaderHiddenOnScroll={true}
+      >
+        <View style={styles.main}>
+          <Text style={styles.temp}>{temp}°</Text>
+          <View style={styles.weatherWrap}>
+            <Text style={styles.weather}>{weather}</Text>
+            <Image
+              style={styles.weatherIcon}
+              source={{ uri: weatherIconUrl }}
+            />
+          </View>
         </View>
-      </View>
+        <View style={styles.main}>
+          {/* 스크롤하는 동안 잠깐 글자색 바뀜 (black -> pink) */}
+          <Text style={[tempColor, styles.temp]}>{temp}°</Text>
+          <View style={styles.weatherWrap}>
+            <Text style={styles.weather}>{weather}</Text>
+            <Image
+              style={styles.weatherIcon}
+              source={{ uri: weatherIconUrl }}
+            />
+          </View>
+        </View>
+        <View style={styles.main}>
+          <Text style={styles.temp}>{temp}°</Text>
+          <View style={styles.weatherWrap}>
+            <Text style={styles.weather}>{weather}</Text>
+            <Image
+              style={styles.weatherIcon}
+              source={{ uri: weatherIconUrl }}
+            />
+          </View>
+        </View>
+        <View style={styles.main}>
+          <Text style={styles.temp}>{temp}°</Text>
+          <View style={styles.weatherWrap}>
+            <Text style={styles.weather}>{weather}</Text>
+            <Image
+              style={styles.weatherIcon}
+              source={{ uri: weatherIconUrl }}
+            />
+          </View>
+        </View>
+        <View style={styles.main}>
+          <Text style={styles.temp}>{temp}°</Text>
+          <View style={styles.weatherWrap}>
+            <Text style={styles.weather}>{weather}</Text>
+            <Image
+              style={styles.weatherIcon}
+              source={{ uri: weatherIconUrl }}
+            />
+          </View>
+        </View>
+      </ScrollView>
       <View style={styles.detailWrap}>
         <View style={styles.detailRow}>
           <Text>최고 {tempMax}°</Text>
@@ -64,3 +140,70 @@ export default function App() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.primary,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  contentContainer: {
+    padding: 4,
+  },
+  city: {
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  dates: {
+    flex: 1.2,
+    margin: 24,
+    borderBottomWidth: 3,
+  },
+  day: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  main: {
+    flex: 10,
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+    padding: 24,
+    margin: 4,
+    borderBottomWidth: 3,
+  },
+  temp: {
+    fontSize: 100,
+    fontWeight: '700',
+  },
+  initialTemp: {
+    color: 'black',
+  },
+  updatedTemp: {
+    color: 'pink',
+  },
+  weatherWrap: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  weather: {
+    fontSize: 20,
+  },
+  weatherIcon: {
+    width: 50,
+    height: 50,
+    marginTop: 8,
+  },
+  detailWrap: {
+    flex: 2,
+    margin: 24,
+  },
+  detailRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
